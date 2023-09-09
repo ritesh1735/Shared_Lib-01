8c358bc3dab4ed43f515d0b89d197a2d26b065d6
Ritesh <142470877+ritesh1735@users.noreply.github.com>
2023-09-09 20:35:16 +0530

    Update cleanWorkspace.groovy

--- vars/cleanWorkspace.groovy
+++ vars/cleanWorkspace.groovy
@@ -1,5 +1,16 @@
-// cleanWorkspace.groovy
-def call() {
-    cleanWs()
+// Shared_Lib-01/vars/myStages.groovy
+
+def myGitHubStage() {
+    stage('CleanWorkspace') {
+        steps {
+            cleanWs()
+            // Add other steps
+        }
+    }
 }
+
+// // cleanWorkspace.groovy
+// def call() {
+//     cleanWs()
+// }
 
