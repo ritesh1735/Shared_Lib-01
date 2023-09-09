978c5acdbc7210882278b81f4f0cb89179a1a708
Ritesh <142470877+ritesh1735@users.noreply.github.com>
2023-09-09 20:42:51 +0530

    Update CloneRepo.groovy

--- vars/CloneRepo.groovy
+++ vars/CloneRepo.groovy
@@ -1,8 +1,13 @@
 // checkout.groovy
 def call(String branch, String repoUrl) {
-    stage('Checkout') {
-        steps {
-            git branch: branch, url: repoUrl
-        }
-    }
+    git branch: branch, url: repoUrl
 }
+
+// // checkout.groovy
+// def call(String branch, String repoUrl) {
+//     stage('Checkout') {
+//         steps {
+//             git branch: branch, url: repoUrl
+//         }
+//     }
+// }
