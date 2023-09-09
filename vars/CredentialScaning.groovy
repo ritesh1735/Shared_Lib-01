ef735cc8435c56d9dbc8ffa5fbc73f4cfa3f3917
Ritesh <142470877+ritesh1735@users.noreply.github.com>
2023-09-09 22:07:56 +0530

    Update CredentialScaning.groovy

--- vars/CredentialScaning.groovy
+++ vars/CredentialScaning.groovy
@@ -1,27 +1,35 @@
 // CredentialScaning.groovy
 
 def call() {
-    sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
-    sh 'chmod +x gitleaks'
-    sh 'sudo mv gitleaks /usr/local/bin/'
-    sh 'gitleaks --report-format=toml > .gitleaks.toml'
-    sh '''
-        echo '[[rules]]
-        pattern = "api_key"
-        exclude = ["*.md"]
-        severity = "high"
+    def gitleaksInstalled = fileExists('/usr/local/bin/gitleaks')
 
-        [[rules]]
-        pattern = "password"
-        commit = true' > .gitleaks.toml
-    '''
-    sh 'cat .gitleaks.toml'
+    if (!gitleaksInstalled) {
+        sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
+        sh 'chmod +x gitleaks'
+        sh 'sudo mv gitleaks /usr/local/bin/'
+    }
+
+    sh 'gitleaks --version' // Check Gitleaks version to verify installation
+
+    if (!gitleaksInstalled) {
+        sh 'gitleaks --report-format=toml > .gitleaks.toml'
+        sh '''
+            echo '[[rules]]
+            pattern = "api_key"
+            exclude = ["*.md"]
+            severity = "high"
+
+            [[rules]]
+            pattern = "password"
+            commit = true' > .gitleaks.toml
+        '''
+        sh 'cat .gitleaks.toml'
+    }
+
     sh 'gitleaks detect -p . --files-at-commit=latest .'
 }
 
-// // GitleaksScan.groovy
-
-// def generateAndEditGitleaksConfig() {
+// def call() {
 //     sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
 //     sh 'chmod +x gitleaks'
 //     sh 'sudo mv gitleaks /usr/local/bin/'
@@ -37,73 +45,5 @@ def call() {
 //         commit = true' > .gitleaks.toml
 //     '''
 //     sh 'cat .gitleaks.toml'
-// }
-
-// def performGitleaksScan() {
-//     sh '''pwd
-//     ls -la
-//     '''
 //     sh 'gitleaks detect -p . --files-at-commit=latest .'
 // }
-
-// // def call() {
-// //     stage('Generate, Edit, and Run Gitleaks Scan') {
-// //         steps {
-// //             script {
-// //                 // Generate and edit Gitleaks configuration
-// //                 sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
-// //                 sh 'chmod +x gitleaks'
-// //                 sh 'sudo mv gitleaks /usr/local/bin/'
-// //                 sh 'gitleaks --report-format=toml > .gitleaks.toml'
-// //                 sh '''
-// //                     echo '[[rules]]
-// //                     pattern = "api_key"
-// //                     exclude = ["*.md"]
-// //                     severity = "high"
-
-// //                     [[rules]]
-// //                     pattern = "password"
-// //                     commit = true' > .gitleaks.toml
-// //                 '''
-// //                 sh 'cat .gitleaks.toml'
-                
-// //                 // Perform the Gitleaks scan
-// //                 sh '''pwd
-// //                 ls -la
-// //                 '''
-// //                 sh 'gitleaks detect -p . --files-at-commit=latest .'
-// //             }
-// //         }
-// //     }
-// // }
-// // // // gitleaksPipeline.groovy
-// // // def call() {
-// // //     stage('Generate, Edit, and Run Gitleaks Scan') {
-// // //         steps {
-// // //             script {
-// // //                 // Generate and edit Gitleaks configuration
-// // //                 sh 'wget https://github.com/zricethezav/gitleaks/releases/download/v7.0.2/gitleaks-linux-amd64 -O gitleaks'
-// // //                 sh 'chmod +x gitleaks'
-// // //                 sh 'sudo mv gitleaks /usr/local/bin/'
-// // //                 sh 'gitleaks --report-format=toml > .gitleaks.toml'
-// // //                 sh '''
-// // //                     echo '[[rules]]
-// // //                     pattern = "api_key"
-// // //                     exclude = ["*.md"]
-// // //                     severity = "high"
-
-// // //                     [[rules]]
-// // //                     pattern = "password"
-// // //                     commit = true' > .gitleaks.toml
-// // //                 '''
-// // //                 sh 'cat .gitleaks.toml'
-                
-// // //                 // Perform the Gitleaks scan
-// // //                 sh '''pwd
-// // //                 ls -la
-// // //                 '''
-// // //                 sh 'gitleaks detect -p . --files-at-commit=latest .'
-// // //             }
-// // //         }
-// // //     }
-// // // }
