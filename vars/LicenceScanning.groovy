31053ecdf86e17a90294d1f15cca9609589e68d3
Ritesh <142470877+ritesh1735@users.noreply.github.com>
2023-09-09 21:14:49 +0530

    Update LicenceScanning.groovy

--- vars/LicenceScanning.groovy
+++ vars/LicenceScanning.groovy
@@ -1,11 +1,9 @@
 // installScancodeAndPerform.groovy
 def call() {
-    stage('Install scancode and perform') {
-        steps {
             sh 'sudo pip install scancode-toolkit'
             sh 'scancode . --json reportfile.json'
             sh 'echo $PATH'
             sh 'which scancode'
         }
-    }
-}
+    
+
