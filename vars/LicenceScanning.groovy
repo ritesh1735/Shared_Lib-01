9be272415fc86c63655188fb0f0d5d87f40ff82b
Ritesh <142470877+ritesh1735@users.noreply.github.com>
2023-09-18 00:44:12 +0530

    Update LicenceScanning.groovy

--- vars/LicenceScanning.groovy
+++ vars/LicenceScanning.groovy
@@ -1,21 +1,38 @@
-def call() {
-    def scancodeInstalled = fileExists('$(which scancode)')
+package org.<your-shared-library-name>
 
-    if (!scancodeInstalled) {
-        sh 'sudo pip install scancode-toolkit'
+class LicenseScanner {
+    def run() {
+        script {
+            def scancodeInstalled = sh(script: 'command -v scancode', returnStatus: true) == 0
+            if (!scancodeInstalled) {
+                sh 'sudo pip install scancode-toolkit'
+            }
+
+            sh 'scancode . --json reportfile.json'
+            sh 'echo $PATH'
+            sh 'which scancode'
+        }
     }
-
-    sh 'scancode . --json reportfile.json'
-    sh 'echo $PATH'
-    sh 'which scancode'
 }
 
-// // installScancodeAndPerform.groovy
 // def call() {
-//             sh 'sudo pip install scancode-toolkit'
-//             sh 'scancode . --json reportfile.json'
-//             sh 'echo $PATH'
-//             sh 'which scancode'
-//         }
+//     def scancodeInstalled = fileExists('$(which scancode)')
+
+//     if (!scancodeInstalled) {
+//         sh 'sudo pip install scancode-toolkit'
+//     }
+
+//     sh 'scancode . --json reportfile.json'
+//     sh 'echo $PATH'
+//     sh 'which scancode'
+// }
+
+// // // installScancodeAndPerform.groovy
+// // def call() {
+// //             sh 'sudo pip install scancode-toolkit'
+// //             sh 'scancode . --json reportfile.json'
+// //             sh 'echo $PATH'
+// //             sh 'which scancode'
+// //         }
     
 
